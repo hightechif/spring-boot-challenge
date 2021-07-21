3fd3491a611e2eecd443be0255420531db8ad10f
R. Fadhil <ridhanfadhilah@gmail.com>
2021-07-22 00:02:32 +0700

    Implement Spring Boot + JSON Web Token Security

---
+++ src/main/java/com/tmt/challenge/config/SpringSecurityConfiguration.java
@@ -0,0 +1,73 @@
+package com.tmt.challenge.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfiguration  extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    CustomUserDetailsService userDetailsService;
+
+    /**
+     * Configure HTTP Security to make use the CustomJwtAuthenticationFiler
+     */
+    @Autowired
+    private CustomJwtAuthenticationFilter customJwtAuthenticationFilter;
+
+    /**
+     * Configure HTTP Security to make use the JwtAuthenticationEntryPoint
+     */
+    @Autowired
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    /**
+     * Authentication Manager
+     * */
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        // We don't need CSRF for this example
+        httpSecurity.csrf().disable()
+                .authorizeRequests().antMatchers("/helloadmin").hasRole("ADMIN")
+                .antMatchers("/hellouser").hasAnyRole("ADMIN","USER")
+                .antMatchers("/authenticate").permitAll().anyRequest().authenticated()   // set authenticate endpoint to be public access
+                // if any exception occurs call this
+                .and().exceptionHandling()
+                .authenticationEntryPoint(unauthorizedHandler).and().
+                // make sure we use stateless session; session won't be used to
+                // store user's state.
+                        sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+                // Add a filter to validate the tokens with every request
+                httpSecurity.addFilterBefore(customJwtAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter.class);
+    }
+
+}
---
+++ src/main/java/com/tmt/challenge/controller/AuthenticationController.java
@@ -0,0 +1,50 @@
+package com.tmt.challenge.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tmt.challenge.config.CustomUserDetailsService;
+import com.tmt.challenge.config.JwtUtil;
+import com.tmt.challenge.model.AuthenticationRequest;
+import com.tmt.challenge.model.AuthenticationResponse;
+
+@RestController
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtTokenUtil;
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
+            throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+        final String token = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(token));
+    }
+
+}
