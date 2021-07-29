97dce4175771be6991e9576b3dc6023f9c050589
R. Fadhil <ridhanfadhilah@gmail.com>
2021-07-29 11:09:36 +0700

    Refresh Token Fixed

--- src/main/java/com/tmt/challenge/controllers/AuthController.java
+++ src/main/java/com/tmt/challenge/controllers/AuthController.java
@@ -15,6 +15,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -49,20 +50,15 @@ public class AuthController {
 
     @RequestMapping(value = "/signin", method = RequestMethod.POST)
     public ResponseEntity<?> createAuthenticationToken(@RequestBody UserLoginDTO userLoginDTO) throws Exception {
-        try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                    userLoginDTO.getUsername(), userLoginDTO.getPassword()));
-        } catch (DisabledException e) {
-            throw new Exception("USER_DISABLED", e);
-        } catch (BadCredentialsException e) {
-            throw new Exception("INVALID_CREDENTIALS", e);
-        }
+
+        Authentication authMngr = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                userLoginDTO.getUsername(), userLoginDTO.getPassword()));
 
 //        refreshTokenService.deleteByUsername(userLoginDTO.getUsername());
 
-        final UserDetails userDetails = userDetailsService.loadUserByUsername(userLoginDTO.getUsername());
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userLoginDTO.getUsername());
 
-        final String jwtToken = jwtUtils.generateJwtToken(userDetails);     // Bearer Token
+        String jwtToken = jwtUtils.generateJwtToken(userDetails);     // Bearer Token
 
         List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority())
                 .collect(Collectors.toList());
