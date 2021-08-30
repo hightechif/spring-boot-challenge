7d62165a2d0883436ef191d08aa7a424950d058d
R. Fadhil <ridhanfadhilah@gmail.com>
2021-08-30 10:47:43 +0700

    fix code

--- src/main/java/com/tmt/challenge/controller/StudentController.java
+++ src/main/java/com/tmt/challenge/controller/StudentController.java
@@ -24,7 +24,7 @@ public class StudentController {
     private final StudentService studentService;
 
     @Autowired
-    public StudentController(StudentService studentService, KafkaSenderService kafkaSenderService) {
+    public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
